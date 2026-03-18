c322d6244586e5eba1f88fc54bca14af30247146
Martin Hauser <mhauser@netboxlabs.com>
2026-03-18 13:14:05 +0100

    refactor(api): Modernize serializers with brief fields
    Replaces WritableNestedSerializer with native nested parameter on
    serializers. Adds brief_fields to Meta classes, includes description
    field in AccessListRuleSerializer, and improves query optimization.

--- netbox_access_lists/api/views.py
+++ netbox_access_lists/api/views.py
@@ -1,5 +1,4 @@
 from django.db.models import Count
-
 from netbox.api.viewsets import NetBoxModelViewSet
 
 from .. import filtersets, models
@@ -7,15 +6,13 @@ from .serializers import AccessListSerializer, AccessListRuleSerializer
 
 
 class AccessListViewSet(NetBoxModelViewSet):
-    queryset = models.AccessList.objects.prefetch_related('tags').annotate(
-        rule_count=Count('rules')
-    )
+    queryset = models.AccessList.objects.annotate(rule_count=Count('rules')).prefetch_related('tags')
     serializer_class = AccessListSerializer
 
 
 class AccessListRuleViewSet(NetBoxModelViewSet):
-    queryset = models.AccessListRule.objects.prefetch_related(
-        'access_list', 'source_prefix', 'destination_prefix', 'tags'
-    )
+    queryset = models.AccessListRule.objects.select_related(
+        'access_list', 'source_prefix', 'destination_prefix'
+    ).prefetch_related('tags')
     serializer_class = AccessListRuleSerializer
     filterset_class = filtersets.AccessListRuleFilterSet
